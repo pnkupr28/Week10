2a405f0d3b65b56b0655ae87f3e36cc94efff404
Vijayanta Jain <vijayantajain@gmail.com>
2024-11-04 12:55:09 -0500

    Add initial files

---
+++ simple_math.py
@@ -0,0 +1,13 @@
+"""File for simple math functions"""
+
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+
+def sub(num_1, num_2):
+    return num_1 - num_2
+
+
+def multi(num_1, num_2):
+    return num_1 * num_2
